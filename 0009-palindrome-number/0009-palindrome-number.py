aab105688ceb6fa90febdcd73b55406f8ca2a419
Tanisha Agrawal <ag.tanisha06@gmail.com>
2026-08-16 13:35:43 +0530

    Time: 16 ms (30.01%), Space: 12.3 MB (90.31%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        
+        temp = x
+        rev = 0
+
+        while temp > 0:
+            rem = temp % 10
+            temp //= 10
+
+            rev = rev*10 + rem
+
+        return x == rev
