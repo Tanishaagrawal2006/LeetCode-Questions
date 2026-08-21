0f39ff5612e29e2abc6bb70837000e7eaa2164a2
Tanisha Agrawal <ag.tanisha06@gmail.com>
2026-08-21 18:51:33 +0530

    Time: 12 ms (83.52%), Space: 12.7 MB (75.71%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def isPalindrome(self, s):
+
+        left = 0
+        right = len(s) - 1
+        s = s.lower()
+
+        while left < right:
+
+            while left < right and not s[left].isalnum():
+                left += 1
+                continue
+
+            while left < right and not s[right].isalnum():
+                right -= 1
+                continue
+
+            if s[left] != s[right]:
+               return False
+            
+            left += 1
+            right -=1
+                
+        return True
+
+                
+
+            
+        
+
+        
