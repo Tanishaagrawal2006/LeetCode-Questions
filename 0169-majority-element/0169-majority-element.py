c316b05dec46083847c85e24a7f2c870a20d2b83
Tanisha Agrawal <ag.tanisha06@gmail.com>
2026-09-02 23:43:35 +0530

    Time: 15 ms (27.93%), Space: 13.5 MB (73.93%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        
+        freq = {}
+
+        for num in nums:
+            if num in freq:
+                freq[num] +=1
+            else:
+                freq[num] = 1
+
+            if freq[num] > len(nums)/2:
+                return num
+        
